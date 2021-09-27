4489f1b0c7294a01781efae729cb5e43fb0faa35
r2dev2 <ronak.badhe@gmail.com>
2021-09-27 10:27:20 -0700

    have made many boxes

--- index.js
+++ index.js
@@ -1,16 +1,17 @@
 /**
  * Conways Game of Life but with cows in vr
  */
+import { board } from './store.js';
 
 const app = document.querySelector('#app');
 const scene = document.createElement('a-scene');
-const createPosition = (x, y, z='0') => [x, y, z].map(x => `${x}`).join(' ');
+const createPosition = (x, y) => `${x / 8} 1.2 ${-y / 8}`;
 const createCow = (x, y) => {
   const cow = document.createElement('a-box');
   cow.setAttribute('position', createPosition(x, y));
-  cow.setAttribute('position', '-1 0.5 -3');
   cow.setAttribute('rotation', '0 0 0');
   cow.setAttribute('color', '#4CC3D9');
+  cow.setAttribute('scale', '.075 .075 .075');
   return cow;
 }
 const createGround = () => {
@@ -22,4 +23,8 @@ const createGround = () => {
   return ground;
 };
 scene.appendChild(createGround());
+const cows = board().map((row, i) => row.map((isFilled, j) => {
+  return createCow(i - 10, j - 10);
+}));
+cows.flat().forEach(cow => scene.appendChild(cow));
 app.appendChild(scene);
---
+++ store.js
@@ -0,0 +1,28 @@
+/**
+ * Makeshift state manager
+ *
+ * @template {T}
+ * @type {(value: T) => [() => T, (newValue: T) => void, (cb: (value: T) => void) => void]}
+ */
+const useState = value => {
+  const subscribers = [];
+  return [
+    function get() { return value; },
+    function set(newValue) {
+      if (newValue == value) return;
+      value = newValue;
+      subscribers.forEach(cb => cb(value));
+    },
+    function subscribe(cb) {
+      subscribers.push(cb);
+      cb(value);
+    }
+  ]
+};
+
+
+export const [rows, setRows, subRows] = useState(20);
+export const [cols, setCols, subCols] = useState(20);
+
+const initialBoard = Array(rows()).fill(Array(cols()).fill(false)).map(x => [...x]);
+export const [board, setBoard, subBoard] = useState(initialBoard);
