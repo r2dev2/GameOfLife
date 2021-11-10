899a680953150d36eb3620e7d2beedef0b128414
r2dev2 <ronak.badhe@gmail.com>
2021-11-09 22:14:25 -0800

    properly handle no amount query parameter you sussy baka

--- store.js
+++ store.js
@@ -28,5 +28,5 @@ const initialBoard = Array(rows()).fill(Array(cols()).fill(false)).map(x => [...
 export const [board, setBoard, subBoard] = useState(initialBoard);
 
 // Random board start
-const threshold = parseFloat(new URLSearchParams(location.search).get('amount')) ?? .5
+const threshold = parseFloat(new URLSearchParams(location.search).get('amount')) || .5
 setBoard(board().map(row => row.map(() => Math.random() < threshold)));
